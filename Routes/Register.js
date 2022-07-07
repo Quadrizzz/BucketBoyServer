ad3ed463c90ca4f707e93fb351c2c0182cae7357
Quadrizz <mackquadrizz@gmail.com>
2022-07-07 01:56:22 +0100

    register

--- Routes/Register.js
+++ Routes/Register.js
@@ -1,6 +1,44 @@
 const register = (req, res, client, bcrypt)=>{
     const {Name, Password, Email} = req.body;
-    const HashedPassword =  bcrypt.hashSync(Password);
+    async function HashPassword(password){
+        const salt = await bcrypt.genSalt(10);
+        const HashedPassword =  await bcrypt.hash(password, salt);
+        return HashedPassword
+    }
+    async function main(){
+        const HashedPassword = await HashPassword(Password);
+        const newUser = {
+            Name : Name,
+            Pasword: HashedPassword,
+            Email: Email
+        }
+        try{
+            await client.connect();
+            await createNewUser(newUser)
+        }
+        catch(e){
+            console.log(e);
+        }
+        finally{
+            await client.close();
+        }
+    }
+    
+    main().catch(console.error)
+    
+    async function createNewUser(newUser){
+        try{
+            const result = await client.db("Kiosk").collection("Users").insertOne(newUser)
+            res.status(200).json(result.insertedId);
+        }
+        catch(e){
+            console.log(e)
+            res.status(400).json("An Error Ocurred")
+        }
+
+    }
+
+
 }
 
 module.exports = {
